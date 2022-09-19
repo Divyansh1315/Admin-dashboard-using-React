c332252ec8ee0a104f7b2556c29fbf88918b572b
DV <divyanshsingh372@gmail.com>
2022-09-19 12:32:32 +0530

    Sidebar.jsx gupdate

--- admin_dashboard/src/components/Sidebar.jsx
+++ admin_dashboard/src/components/Sidebar.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import { SiShopware } from "react-icons/si";
+import { MdOutlineCancel } from "react-icons/md";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
 const Sidebar = () => {
-  return <div></div>;
+  return;
+  <div></div>;
 };
 
 export default Sidebar;
